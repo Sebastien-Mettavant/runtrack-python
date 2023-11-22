4e1ba7e8b7f6fd738c5001714bec45255f7797e0
Sebastien Mettavant <sebastien.mettavant@laplateforme.io>
2023-11-22 16:25:05 +0100

    Update

---
+++ jour03/job06/main.py
@@ -0,0 +1,12 @@
+number = 4
+o = 0
+def research(number):
+    if number < o:
+        print("neg")
+    elif number > o:
+        print("pos")
+    else:
+        print("zero")
+research(number)
+#plus qu a ghanger < ou > par autres 
+
